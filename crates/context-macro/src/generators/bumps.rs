ff18abacb2583276ebf23384337a47d7bfef2d41
otpfmabtmksfrk6931CesarWolfe <dolce_petter0@icloud.com>
2025-04-18 11:43:02 +0700

    Add ATA pda check (#135)
    * Add ATA pda check
    
    * Fix lints

--- crates/context-macro/src/generators/bumps.rs
+++ crates/context-macro/src/generators/bumps.rs
@@ -3,42 +3,46 @@ use {
     crate::{
         accounts::Account,
         constraints::{
-            ConstraintBump, ConstraintInitIfNeeded, ConstraintProgram, ConstraintSeeded,
-            ConstraintSeeds,
+            ConstraintAssociatedToken, ConstraintBump, ConstraintInitIfNeeded, ConstraintProgram,
+            ConstraintSeeded, ConstraintSeeds,
         },
         context::Context,
-        extractor::InnerTyExtractor,
         visitor::ContextVisitor,
     },
+    proc_macro2::TokenStream,
     quote::{format_ident, quote},
-    syn::{parse_quote, punctuated::Punctuated, visit::Visit, Expr, Ident, PathSegment, Token},
+    syn::{parse_quote, punctuated::Punctuated, Expr, Ident, Token},
 };
 
-// TODO change to add builder by account
-#[derive(Default)]
-pub struct BumpsGenerator {
-    context_name: Option<String>,
-    account: Option<(Ident, PathSegment)>,
-    bump: Option<Expr>,
-    init_if_needed: bool,
-    is_seeded: bool,
-    seeds: Option<Punctuated<Expr, Token![,]>>,
-    program_id: Option<Expr>,
-    result: GeneratorResult,
-    struct_fields: Vec<Ident>,
+pub enum PdaType {
+    Default,
+    Seeded,
+    Ata,
 }
 
-impl BumpsGenerator {
-    pub fn new() -> Self {
-        BumpsGenerator::default()
+pub struct PdaGenerator<'a> {
+    account: &'a Account,
+    init_if_needed: bool,
+    pda_ty: PdaType,
+    seeds: Option<Punctuated<Expr, Token![,]>>,
+    program_id: Option<Expr>,
+    bump: Option<Expr>,
+}
+
+impl<'a> PdaGenerator<'a> {
+    pub fn new(account: &'a Account) -> Self {
+        PdaGenerator {
+            account,
+            pda_ty: PdaType::Default,
+            seeds: None,
+            program_id: None,
+            bump: None,
+            init_if_needed: false,
+        }
     }
 
-    pub fn is_pda(&self) -> bool {
-        self.is_seeded || self.seeds.is_some()
-    }
-
-    fn extend_checks(&mut self) -> Result<(), syn::Error> {
-        let (name, ty) = self.account.as_ref().unwrap();
+    pub fn generate(self) -> Result<(TokenStream, bool), syn::Error> {
+        let name = &self.account.name;
         let pda_key = format_ident!("{}_key", name);
         let pda_bump = format_ident!("{}_bump", name);
         let program_id = self
@@ -48,7 +52,7 @@ impl BumpsGenerator {
             .unwrap_or(quote!(crate::ID));
 
         let (pda, seeds) = if let Some(bump) = &self.bump {
-            let seeds_token = if self.is_seeded {
+            let seeds_token = if matches!(self.pda_ty, PdaType::Seeded) {
                 quote!(#name.data()?.seeds_with_bump(&[#pda_bump]))
             } else {
                 let seeds = self.seeds.as_ref().ok_or(syn::Error::new(
@@ -71,13 +75,8 @@ impl BumpsGenerator {
                 "Seeds constraint is not specified.",
             ))?;
 
-            let seeds = if self.is_seeded {
-                let mut inner_ty_extractor = InnerTyExtractor::new();
-                inner_ty_extractor.visit_path_segment(ty);
-                let inner_ty_str = inner_ty_extractor
-                    .ty
-                    .ok_or(syn::Error::new(name.span(), "Cannot find the inner type."))?;
-                let inner_ty = format_ident!("{inner_ty_str}");
+            let seeds = if matches!(self.pda_ty, PdaType::Seeded) {
+                let inner_ty = format_ident!("{}", self.account.inner_ty);
 
                 quote!(#inner_ty::derive(#keys))
             } else {
@@ -105,14 +104,108 @@ impl BumpsGenerator {
             quote!(#pda)
         };
 
-        self.result.at_init.extend(quote! {
-            #pda_assign
-            if #name.key() != &#pda_key {
-                return Err(ProgramError::InvalidSeeds);
-            }
-        });
+        Ok((
+            quote! {
+                #pda_assign
+                if #name.key() != &#pda_key {
+                    return Err(ProgramError::InvalidSeeds);
+                }
+            },
+            self.bump.is_none() || self.init_if_needed,
+        ))
+    }
+}
+
+impl ContextVisitor for PdaGenerator<'_> {
+    fn visit_program(&mut self, constraint: &ConstraintProgram) -> Result<(), syn::Error> {
+        self.program_id = Some(constraint.0.clone());
 
         Ok(())
+    }
+
+    fn visit_bump(&mut self, constraint: &ConstraintBump) -> Result<(), syn::Error> {
+        self.bump = constraint.0.clone();
+
+        Ok(())
+    }
+
+    fn visit_seeded(&mut self, constraint: &ConstraintSeeded) -> Result<(), syn::Error> {
+        if matches!(self.pda_ty, PdaType::Default | PdaType::Ata) && self.seeds.is_some() {
+            return Err(syn::Error::new(
+                self.account.name.span(),
+                "Cannot specified keys and seeds at the same time.",
+            ));
+        }
+
+        self.pda_ty = PdaType::Seeded;
+        self.seeds = constraint.0.clone();
+
+        Ok(())
+    }
+
+    fn visit_seeds(&mut self, constraint: &ConstraintSeeds) -> Result<(), syn::Error> {
+        if matches!(self.pda_ty, PdaType::Seeded | PdaType::Ata) {
+            return Err(syn::Error::new(
+                self.account.name.span(),
+                "Cannot specified keys and seeds at the same time.",
+            ));
+        } //TODO change it in cross check
+
+        self.seeds = Some(constraint.seeds.clone());
+
+        Ok(())
+    }
+
+    fn visit_init_if_needed(
+        &mut self,
+        _constraint: &ConstraintInitIfNeeded,
+    ) -> Result<(), syn::Error> {
+        self.init_if_needed = true;
+
+        Ok(())
+    }
+
+    fn visit_associated_token(
+        &mut self,
+        constraint: &ConstraintAssociatedToken,
+    ) -> Result<(), syn::Error> {
+        self.pda_ty = PdaType::Ata;
+
+        match constraint {
+            ConstraintAssociatedToken::Mint(ident) => {
+                if let Some(seeds) = self.seeds.as_mut() {
+                    seeds.insert(2, parse_quote!(#ident));
+                } else {
+                    self.seeds = Some(parse_quote!(token_program.key().as_ref(), #ident));
+                    self.program_id = Some(parse_quote!(AtaTokenProgram::ID))
+                }
+            }
+            ConstraintAssociatedToken::Authority(ident) => {
+                if let Some(seeds) = self.seeds.as_mut() {
+                    seeds.insert(0, parse_quote!(#ident));
+                } else {
+                    self.seeds = Some(parse_quote!(#ident, token_program.key().as_ref()));
+                    self.program_id = Some(parse_quote!(AtaTokenProgram::ID))
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+// TODO change to add builder by account
+#[derive(Default)]
+pub struct BumpsGenerator {
+    context_name: Option<String>,
+    is_pda: bool,
+    result: GeneratorResult,
+    struct_fields: Vec<Ident>,
+}
+
+impl BumpsGenerator {
+    pub fn new() -> Self {
+        BumpsGenerator::default()
     }
 }
 
@@ -151,83 +244,35 @@ impl ConstraintGenerator for BumpsGenerator {
 }
 
 impl ContextVisitor for BumpsGenerator {
-    fn visit_program(&mut self, constraint: &ConstraintProgram) -> Result<(), syn::Error> {
-        self.program_id = Some(constraint.0.clone());
+    fn visit_context(&mut self, context: &Context) -> Result<(), syn::Error> {
+        self.context_name = Some(context.item_struct.ident.to_string());
+        self.visit_accounts(&context.accounts)?;
 
         Ok(())
     }
 
-    fn visit_context(&mut self, context: &Context) -> Result<(), syn::Error> {
-        self.context_name = Some(context.item_struct.ident.to_string());
-
-        self.visit_accounts(&context.accounts)?;
-
-        if let Some(args) = &context.args {
-            self.visit_arguments(args)?;
-        }
+    fn visit_bump(&mut self, _constraint: &ConstraintBump) -> Result<(), syn::Error> {
+        self.is_pda = true;
 
         Ok(())
     }
 
     fn visit_account(&mut self, account: &Account) -> Result<(), syn::Error> {
-        self.account = Some((account.name.clone(), account.ty.clone()));
-        self.bump = None;
-        self.is_seeded = false;
-        self.seeds = None;
-        self.init_if_needed = false;
-
         self.visit_constraints(&account.constraints)?;
 
-        if self.is_pda() {
-            self.extend_checks()?;
+        let mut pda_generator = PdaGenerator::new(account);
+        pda_generator.visit_account(account)?;
+
+        if self.is_pda {
+            let (token, is_field_generated) = pda_generator.generate()?;
+
+            if is_field_generated {
+                self.struct_fields.push(account.name.clone());
+            }
+            self.result.at_init.extend(token);
+
+            self.is_pda = false;
         }
-
-        Ok(())
-    }
-
-    fn visit_bump(&mut self, constraint: &ConstraintBump) -> Result<(), syn::Error> {
-        self.bump = constraint.0.clone();
-
-        if self.bump.is_none() || self.init_if_needed {
-            self.struct_fields
-                .push(self.account.as_ref().unwrap().0.clone());
-        }
-
-        Ok(())
-    }
-
-    fn visit_seeded(&mut self, constraint: &ConstraintSeeded) -> Result<(), syn::Error> {
-        if !self.is_seeded && self.seeds.is_some() {
-            return Err(syn::Error::new(
-                self.account.as_ref().unwrap().0.span(),
-                "Cannot specified keys and seeds at the same time.",
-            ));
-        }
-
-        self.is_seeded = true;
-        self.seeds = constraint.0.clone();
-
-        Ok(())
-    }
-
-    fn visit_seeds(&mut self, constraint: &ConstraintSeeds) -> Result<(), syn::Error> {
-        if self.is_seeded {
-            return Err(syn::Error::new(
-                self.account.as_ref().unwrap().0.span(),
-                "Cannot specified keys and seeds at the same time.",
-            ));
-        }
-
-        self.seeds = Some(constraint.seeds.clone());
-
-        Ok(())
-    }
-
-    fn visit_init_if_needed(
-        &mut self,
-        _constraint: &ConstraintInitIfNeeded,
-    ) -> Result<(), syn::Error> {
-        self.init_if_needed = true;
 
         Ok(())
     }
